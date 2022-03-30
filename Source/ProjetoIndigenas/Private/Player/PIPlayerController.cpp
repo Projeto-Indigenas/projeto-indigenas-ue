588d958efda91eaac55b82e4fc51160d6b80bce2
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-03-30 01:00:30 -0300

    Organizing the project;
    Added placeholder player controller;

---
+++ Source/ProjetoIndigenas/Private/Player/PIPlayerController.cpp
@@ -0,0 +1,2 @@
+#include "Player/PIPlayerController.h"
+
--- Source/ProjetoIndigenas/Public/ProjetoIndigenasGameModeBase.h
+++ Source/ProjetoIndigenas/Public/ProjetoIndigenasGameModeBase.h
@@ -1,14 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "ProjetoIndigenasGameModeBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class PROJETOINDIGENAS_API AProjetoIndigenasGameModeBase : public AGameModeBase
 {
