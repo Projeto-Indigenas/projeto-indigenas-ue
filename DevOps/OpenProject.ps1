d5975e9c5ec3877661968fd958ce01afee74fd22
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-03-30 23:44:27 -0300

    Scripts for better work flow;

---
+++ DevOps/OpenProject.ps1
@@ -0,0 +1,4 @@
+. ".\DevOps\Library.ps1"
+
+Write-Output "Opening project: $env:PROJECT_PATH"
+Start-Process "$env:PROJECT_PATH"
--- DevOps/RefreshProject.ps1
+++ DevOps/RefreshProject.ps1
@@ -1,22 +1,4 @@
-using namespace System.IO
+. ".\DevOps\Library.ps1"
 
-Write-Output "Using engine at: $env:UE_ENGINE_ROOT"
-$unrealBuildToolPath = [Path]::Combine($env:UE_ENGINE_ROOT, "Engine", "Binaries", "DotNET", "UnrealBuildTool.exe")
-Write-Output "Using project at: $env:UE_PROJECT_ROOT"
-$projectPath = [Path]::Combine("$env:UE_PROJECT_ROOT", "ProjetoIndigenas.uproject")
-
-if (![File]::Exists($unrealBuildToolPath))
-{
-    Write-Error "Could not find solution generator at: $unrealBuildToolPath"
-
-    return
-}
-
-if (![File]::Exists($projectPath))
-{
-    Write-Error "Could not find project at: $projectPath"
-
-    return
-}
-
-& $unrealBuildToolPath -projectfiles -project="$projectPath" -game -rocket -progress | Out-Default
+Write-Output "Refreshing project: $env:PROJECT_PATH"
+& $env:UNREAL_BUILD_TOOL -projectfiles -project="$env:PROJECT_PATH" -game -rocket -progress | Out-Default
