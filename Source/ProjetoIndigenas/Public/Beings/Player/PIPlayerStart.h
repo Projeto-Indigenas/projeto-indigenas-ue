7bfe30d3fb81059f656a8998f8bd96fd3a6b6ed6
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-18 00:41:38 -0300

    Fixed issue with AI not moving through the path;
    Optimized the Navigation using Invokers;

---
+++ Source/ProjetoIndigenas/Public/Beings/Player/PIPlayerStart.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "PIPlayerStart.generated.h"
+
+UCLASS()
+class PROJETOINDIGENAS_API APIPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+};
