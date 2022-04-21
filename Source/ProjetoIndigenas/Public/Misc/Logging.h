e8fe4d22e0c7cb819324de5fec448308a6810f50
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-21 04:48:23 -0300

    Fixed compilation error;

--- Source/ProjetoIndigenas/Public/Misc/Logging.h
+++ Source/ProjetoIndigenas/Public/Misc/Logging.h
@@ -9,18 +9,13 @@
 		GEngine->AddOnScreenDebugMessage(GetLogKey(), duration, FColor::Red, message); \
 	}
 
-#define PI_LOG_V_FMT(verbosity, format, ...) \
-const FString& message = FString::Printf(format, ##__VA_ARGS__); \
-PI_LOG_V_MSG(verbosity, *message)
-
 #define PI_LOG_V_MSG(verbosity, message) \
-UE_LOG(LogTemp, verbosity, TEXT("[%s::%s] - %s"), *StaticClass()->GetName(), __func__, message);
+	UE_LOG(LogTemp, verbosity, TEXT("[%s::%s] - %s"), *StaticClass()->GetName(), __func__, message);
+#define PI_LOG_MSG(message) PI_LOG_V_MSG(Log, message)
 
-#define PI_LOG_FMT(format, ...) \
-PI_LOG_V_FMT(Log, format, ##__VA_ARGS__)
-
-#define PI_LOG_MSG(message) \
-PI_LOG_V_MSG(Log, message)
+#define PI_LOG_V_FMT(verbosity, format, ...) \
+	PI_LOG_V_MSG(verbosity, *FString::Printf(format, ##__VA_ARGS__))
+#define PI_LOG_FMT(format, ...) PI_LOG_V_FMT(Log, format, ##__VA_ARGS__)
 
 #else
 
