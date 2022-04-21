2ac888ec8c940181ed05f356d3c1a3d1ace544da
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-21 00:33:51 -0300

    Configured project target;
    Added trigger box to scene;
    Added custom trigger box class;

---
+++ Source/ProjetoIndigenas/Public/Interactables/PITriggerBox.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PITriggerBox.generated.h"
+
+DECLARE
+
+UCLASS(BlueprintType, Blueprintable)
+class PROJETOINDIGENAS_API UPITriggerBox : public UTriggerBox
+{
+    GENERATED_BODY()
+
+
+};
