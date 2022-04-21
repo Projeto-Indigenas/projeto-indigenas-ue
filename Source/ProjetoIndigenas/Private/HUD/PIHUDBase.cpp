ff1e3ce17a4b5b11ba7287c29f2da8847a9fe314
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-21 17:20:16 -0300

    Creating the HUDBase with transition abilities;

---
+++ Source/ProjetoIndigenas/Private/HUD/PIHUDBase.cpp
@@ -0,0 +1,24 @@
+#include "HUD/PIHUDBase.h"
+
+#include "Misc/Logging.h"
+
+void APIHUDBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	_gameFlowSubsystem = GetGameInstance()->GetSubsystem<UPIGameFlowSubsystem>();
+}
+
+void APIHUDBase::TransitionTo(const FName state)
+{
+	if (!_gameFlowSubsystem.IsValid())
+	{
+		PI_LOGV_UOBJECT(Error, TEXT("Somehow the Game flow subsystem is invalid"))
+
+		return;
+	}
+	
+	_gameFlowSubsystem->TransitionTo(state);
+}
+
+
