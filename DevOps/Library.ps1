d2dfb3739bde3216262348e522dddb6a1c6f52a9
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-20 23:28:55 -0300

    Feature/support macos (#17)
    * Making the refresh command work on MacOS;
    
    * Configured project to work in VSCode;
    
    * Making Library support both mac and windows;

--- DevOps/Library.ps1
+++ DevOps/Library.ps1
@@ -1,8 +1,20 @@
 using namespace System.IO
 
+if ([string]::IsNullOrEmpty($env:UE_ENGINE_ROOT)) {
+    if ($IsMacOS) {
+        $env:UE_ENGINE_ROOT = "/Users/Shared/Epic Games/UE_5.0/"
+    } else {
+        $env:UE_ENGINE_ROOT = "%PROGRAMFILES%/Epic Games/UE_5.0/"
+    }
+}
+
 $env:PROJECT_ROOT = "$(Get-Location)"
 $env:PROJECT_PATH = [Path]::Combine("$env:PROJECT_ROOT", "ProjetoIndigenas.uproject")
-$env:UNREAL_BUILD_TOOL = [Path]::Combine($env:UE_ENGINE_ROOT, "Engine", "Binaries", "DotNET", "UnrealBuildTool", "UnrealBuildTool.exe")
+$env:UNREAL_BUILD_TOOL = [Path]::Combine($env:UE_ENGINE_ROOT, "Engine", "Binaries", "DotNET", "UnrealBuildTool", "UnrealBuildTool")
+
+if ($IsWindows) {
+    $env:UNREAL_BUILD_TOOL = [Path]::ChangeExtension($env:UNREAL_BUILD_TOOL, ".exe")
+}
 
 Write-Output "Using engine at: $env:UE_ENGINE_ROOT"
 Write-Output "Using project at: $env:PROJECT_PATH"
--- DevOps/RefreshProject.ps1
+++ DevOps/RefreshProject.ps1
@@ -1,4 +1,4 @@
 . ".\DevOps\Library.ps1"
 
 Write-Output "Refreshing project: $env:PROJECT_PATH"
-& $env:UNREAL_BUILD_TOOL -projectfiles -project="$env:PROJECT_PATH" -game -rocket -progress | Out-Default
+& $env:UNREAL_BUILD_TOOL -projectfiles -project="$env:PROJECT_PATH" -game -rocket -progress $args | Out-Default
