87f11a98ee658695032cca12027d051a2b449624
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-29 10:30:32 -0300

    Changed the build script to have default values;

--- DevOps/BuildProject.ps1
+++ DevOps/BuildProject.ps1
@@ -1,7 +1,48 @@
 . ".\DevOps\Library.ps1"
 
 [List[string]] $params = $args
-RunBuildTool($params)
+
+foreach ($param in $params)
+{
+    if ($param.StartsWith("-target"))
+    {
+        $target = $param;
+    }
+
+    if ($param.StartsWith("-configuration"))
+    {
+        $configuration = $param;
+    }
+
+    if ($param.StartsWith("-platform"))
+    {
+        $platform = $param;
+    }
+}
+
+if ([string]::IsNullOrEmpty($target))
+{
+    $target = "ProjetoIndigenasEditor"
+}
+
+if ([string]::IsNullOrEmpty($configuration))
+{
+    $configuration = "Development"
+}
+
+if ([string]::IsNullOrEmpty($platform))
+{
+    if ($IsMacOS)
+    {
+        $platform = "Mac"
+    }
+    else 
+    {
+        $platform = "Win64"
+    }
+}
+
+RunBuildTool($($target, $configuration, $platform) + $params)
 
 if ($args.Contains("-open"))
 {
