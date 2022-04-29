6496c9ab633bb657584d789ba805d7d4ddca0177
Anderson Lucas C. Ramos <andersonlucasg3@hotmail.com>
2022-04-29 11:02:07 -0300

    Feature/adding raoni to the project (#25)
    * Added new assets from metahuman;
    
    Organizing animations;
    
    * Completely removing Iara from project, now using Child-Iara;
    
    * Fixed disappearing hair;
    
    * Making possible to load different skins for the characters;
    
    * Setting the main asset for character configurations;
    
    * Switching characters, saving and loading ingame;
    
    * Full body configuration;
    
    * Configuring normal weight body for Raoni;
    
    * Updated DLL;
    
    * Updated all assets from Raoni;
    
    * Changed the build script to have default values;
    
    * Fixed array creation;
    
    * Making changes in the scene to include Kauane;
    
    Fixed different character scales;

---
+++ Source/ProjetoIndigenas/Public/Beings/Shared/Config/PICharacterConfigurationSettings.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "PICharacterConfigurationData.h"
+#include "PICharacterConfigurationSettings.generated.h"
+
+UCLASS(Config=Game, DefaultConfig, BlueprintType)
+class PROJETOINDIGENAS_API UPICharacterConfigurationSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, BlueprintReadOnly, EditDefaultsOnly)
+	TSoftObjectPtr<UPICharacterConfigurationData> CharacterConfigurationData;
+
+	UPICharacterConfigurationSettings()
+	{
+		CategoryName = TEXT("1 - Projeto Indígenas");
+	}
+};
